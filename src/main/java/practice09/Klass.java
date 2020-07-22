d5aa2e876db617f7742e062c126c6c78747e4973
LIANGFR2 <372334264@qq.com>
2020-07-22 21:26:30 +0800

    [OO-Step-by-Step] [Front] feature: create Person,Student,Teacher,Klass in practice09, klass can appendMember, student can introduce his class

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,43 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students;
+
+    public Klass(int number) {
+        this.number = number;
+        students = new ArrayList<Student>();
+    }
+    public void appendMember(Student newStudent){
+        students.add(newStudent);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.contains(leader)){
+            this.leader = leader;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+
+    }
+
+    public String getDisplayName(){
+        return "Class "+number;
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,26 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        String extraIntroduce = "I am at Class "+ klass.getNumber() +".";
+        Student leader = klass.getLeader();
+        if (leader!=null && leader.getId() == this.getId()){
+            extraIntroduce = "I am Leader of Class "+ klass.getNumber() +".";
+        }
+        return super.introduce() + " I am a Student. " + extraIntroduce;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
