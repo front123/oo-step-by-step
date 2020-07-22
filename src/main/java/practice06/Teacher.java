38a05cceb048813c2d5b50eabf98e8ee51e84265
LIANGFR2 <372334264@qq.com>
2020-07-22 21:18:28 +0800

    [OO-Step-by-Step] [Front] feature: create Person,Student,Teacher in practice06, override introduce function to return their own information

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String extraIntroduce = " I am a Teacher. I teach No Class.";
+        if (klass != null){
+            extraIntroduce = " I am a Teacher. I teach Class "+ klass +".";
+        }
+        return super.introduce() + extraIntroduce;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
