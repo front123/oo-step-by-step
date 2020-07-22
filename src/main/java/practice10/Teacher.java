03eef1531ec607f466cc20ab1dc086bf5564bc75
LIANGFR2 <372334264@qq.com>
2020-07-22 21:29:21 +0800

    [OO-Step-by-Step] [Front] feature: create Person,Student,Teacher,Klass in practice10, teacher can teach several class and know the student he teachs

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,58 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+import java.util.List;
+
+public class Teacher extends Person{
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.classes = new LinkedList<>();
+    }
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    @Override
+    public String introduce() {
+        String extraIntroduce = "I teach No Class.";
+        if (classes.size()>0){
+            String classNumbers = "";
+            for (Klass klass : classes) {
+                classNumbers += klass.getNumber()+", ";
+            }
+            extraIntroduce = "I teach Class "+ classNumbers.substring(0, classNumbers.length()-2) +".";
+        }
+        return super.introduce() + " I am a Teacher. " + extraIntroduce;
+    }
+
+    public String introduceWith(Student student){
+        Klass studentKlass = student.getKlass();
+        String extraIntroduce = " I don't teach "+ student.getName() +".";
+        for (Klass klass: classes) {
+            if (studentKlass.getNumber() == klass.getNumber()){
+                extraIntroduce = " I teach " + student.getName() + ".";
+                break;
+            }
+        }
+
+        return super.introduce() + " I am a Teacher." + extraIntroduce;
+    }
+
+    public boolean isTeaching(Student student){
+        for (Klass klass: classes) {
+            if (klass.isIn(student)) return true;
+        }
+        return false;
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Klass> classes) {
+        this.classes = classes;
+    }
 }
